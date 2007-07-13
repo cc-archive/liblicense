1411337951ecdb2f62186b39562ba68811f8b8bf
paulproteus <paulproteus@aec4f576-4910-0410-beeb-ecb0427469e7>
2007-07-13 22:22:11 +0000

    I relicensed everything
    git-svn-id: https://cctools.svn.sourceforge.net/svnroot/cctools/liblicense/trunk@6216 aec4f576-4910-0410-beeb-ecb0427469e7

--- bindings/python/tests/test.py
+++ bindings/python/tests/test.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python
 # Creative Commons has made the contents of this file
-# available under a CC-GNU-GPL license:
+# available under a CC-GNU-LGPL license:
 #
-# http://creativecommons.org/licenses/GPL/2.0/
+# http://creativecommons.org/licenses/LGPL/2.1/
 #
 # A copy of the full license can be found as part of this
 # distribution in the file COPYING.
@@ -11,7 +11,7 @@
 # terms of that license. You agree that you are solely 
 # responsible for your use of the liblicense software and you
 # represent and warrant to Creative Commons that your use
-# of the liblicense software will comply with the CC-GNU-GPL.
+# of the liblicense software will comply with the CC-GNU-LGPL.
 #
 # Copyright 2007, Creative Commons, www.creativecommons.org.
 # Copyright 2007, Scott Shawcroft.
