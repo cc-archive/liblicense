772f66fcef6f949575afeb7a542d263feef2bfa8
tannewt <tannewt@aec4f576-4910-0410-beeb-ecb0427469e7>
2007-07-16 19:50:10 +0000

    Replaced public domain option with attribution.  Primitive license selection added.
    git-svn-id: https://cctools.svn.sourceforge.net/svnroot/cctools/liblicense/trunk@6238 aec4f576-4910-0410-beeb-ecb0427469e7

--- src/gnome/widgets.py
+++ src/gnome/widgets.py
@@ -5,21 +5,20 @@ class LicenseWidget(gtk.VBox):
     def __init__(self,filename):
         gtk.VBox.__init__(self)
         
-        # Public Domain
+        # Attribution
         box = gtk.HBox()
         box.show()
         
-        self.pd = gtk.CheckButton()
-        self.pd.show()
-        self.pd.connect("toggled",self.pd_toggled)
-        box.pack_start(self.pd,False,False,10)
+        self.by = gtk.CheckButton()
+        self.by.show()
+        box.pack_start(self.by,False,False,10)
         
         icon = gtk.Image()
-        icon.set_from_file("pd.svg")
+        icon.set_from_file("by.svg")
         icon.show()
         box.pack_start(icon,False,False,10)
 
-        label = gtk.Label("Public Domain")
+        label = gtk.Label("Attribution")
         label.show()
         box.pack_start(label)
         
@@ -50,6 +49,7 @@ class LicenseWidget(gtk.VBox):
         
         self.ar = gtk.CheckButton()
         self.ar.show()
+        self.ar.connect("toggled",self.ar_toggled)
         box.pack_start(self.ar,False,False,10)
         
         icon = gtk.Image()
@@ -111,13 +111,16 @@ class LicenseWidget(gtk.VBox):
         box.pack_start(label)
         
         current_license = liblicense.read(filename)
-        entry = gtk.Entry()
-        version = liblicense.get_version(current_license)
-        entry.set_text(liblicense.get_name(current_license) + " " + str(version[0]) + "." + str(version[1]) + "." + str(version[2]))
-        entry.set_editable(False)
-        entry.set_has_frame(False)
-        entry.show()
-        box.pack_start(entry)
+        self.license = gtk.Entry()
+        if current_license:
+            version = liblicense.get_version(current_license)
+            self.license.set_text(liblicense.get_name(current_license) + " " + str(version[0]) + "." + str(version[1]) + "." + str(version[2]))
+        else:
+            self.license.set_text("none")
+        self.license.set_editable(False)
+        self.license.set_has_frame(False)
+        self.license.show()
+        box.pack_start(self.license)
         
         self.pack_start(box)
         
@@ -129,32 +132,74 @@ class LicenseWidget(gtk.VBox):
         label.show()
         box.pack_start(label)
         
-        current_license = liblicense.read(filename)
         self.update_checkboxes(current_license)
-        entry = gtk.Entry()
-        entry.set_text(current_license)
-        entry.set_editable(True)
-        entry.show()
-        box.pack_start(entry)
+        self.uri = gtk.Entry()
+        if current_license:
+            self.uri.set_text(current_license)
+        self.uri.set_editable(True)
+        self.uri.show()
+        box.pack_start(self.uri)
         self.label = label
         self.icon = icon
         self.pack_start(box)
+        
+        # get list of licenses
+        l = liblicense.get_licenses()
+        self.licenses = {}
+        for license in l:
+            if not self.licenses.has_key(self.license_flags(license)):
+                self.licenses[self.license_flags(license)] = []
+            self.licenses[self.license_flags(license)].append(license)
+        
+        # hook up license switcher
+        self.ash.connect("toggled",self.update_license,0)
+        self.ar.connect("toggled",self.update_license,2)
+        self.pcw.connect("toggled",self.update_license,1)
+        self.sa.connect("toggled",self.update_license,3)
+        self.by.connect("toggled",self.update_license,4)
     
     def update_checkboxes(self,license):
         if license=="http://creativecommons.org/licenses/publicdomain/":
             self.pd.set_active(True)
+            self.pd.toggled()
         else:
-            permits = liblicense.get_permits(license)
-            self.ash.set_active("http://creativecommons.org/ns#Distribution" in permits)
-            self.pcw.set_active("http://creativecommons.org/ns#CommercialUse" in liblicense.get_prohibits(license))
-            self.ar.set_active("http://creativecommons.org/ns#DerivativeWorks" in permits)
-            self.sa.set_active("http://creativecommons.org/ns#ShareAlike" in liblicense.get_requires(license))
+            if license:
+                self.flags= list(self.license_flags(license))
+            else:
+                self.flags=[False,False,False,False,False]
+            self.ash.set_active(self.flags[0])
+            self.pcw.set_active(self.flags[1])
+            self.ar.set_active(self.flags[2])
+            self.ar.toggled()
+            self.sa.set_active(self.flags[3])
+            self.by.set_active(self.flags[4])
+
+    def ar_toggled(self,button):
+        self.sa_box.set_sensitive(button.get_active())
+        if not button.get_active():
+            self.sa.set_active(False)
+            self.sa.toggled()
     
-    def pd_toggled(self,button):
-        self.ash_box.set_sensitive(not button.get_active())
-        self.ar_box.set_sensitive(not button.get_active())
-        self.pcw_box.set_sensitive(not button.get_active())
-        self.sa_box.set_sensitive(not button.get_active())
+    def update_license(self,button,flag):
+        self.flags[flag] = button.get_active()
+        if self.licenses.has_key(tuple(self.flags)):
+            print self.licenses[tuple(self.flags)]
+            u = self.licenses[tuple(self.flags)][0]
+            self.uri.set_text(u)
+            v = liblicense.get_version(u)
+            self.license.set_text(liblicense.get_name(u) + " " + str(v[0]) + "." + str(v[1]) + "." + str(v[2]))
+        else:
+            self.uri.set_text("")
+            self.license.set_text("none")
+    
+    def license_flags(self,license):
+        permits = liblicense.get_permits(license)
+        requires = liblicense.get_requires(license)
+        return ("http://creativecommons.org/ns#Distribution" in permits,
+                "http://creativecommons.org/ns#CommercialUse" in liblicense.get_prohibits(license),
+                "http://creativecommons.org/ns#DerivativeWorks" in permits,
+                "http://creativecommons.org/ns#ShareAlike" in requires,
+                "http://creativecommons.org/ns#Attribution" in requires)
 
 if __name__=="__main__":
     import sys
