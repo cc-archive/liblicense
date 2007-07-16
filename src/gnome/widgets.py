1c4f38a419c1cf03122314eefd5d39853e877acc
tannewt <tannewt@aec4f576-4910-0410-beeb-ecb0427469e7>
2007-07-16 06:20:21 +0000

    Loads current license properties. Run standalone to test widget and pass it a filename.
    git-svn-id: https://cctools.svn.sourceforge.net/svnroot/cctools/liblicense/trunk@6235 aec4f576-4910-0410-beeb-ecb0427469e7

--- src/gnome/widgets.py
+++ src/gnome/widgets.py
@@ -11,12 +11,13 @@ class LicenseWidget(gtk.VBox):
         
         self.pd = gtk.CheckButton()
         self.pd.show()
-        box.pack_start(self.pd)
+        self.pd.connect("toggled",self.pd_toggled)
+        box.pack_start(self.pd,False,False,10)
         
         icon = gtk.Image()
-        icon.set_from_file("/home/scott/.nautilus/python-extensions/pd.svg")
+        icon.set_from_file("pd.svg")
         icon.show()
-        box.pack_start(icon)
+        box.pack_start(icon,False,False,10)
 
         label = gtk.Label("Public Domain")
         label.show()
@@ -30,17 +31,17 @@ class LicenseWidget(gtk.VBox):
         
         self.ash = gtk.CheckButton()
         self.ash.show()
-        box.pack_start(self.ash)
+        box.pack_start(self.ash,False,False,10)
         
         icon = gtk.Image()
-        icon.set_from_file("/home/scott/.nautilus/python-extensions/ash.svg")
+        icon.set_from_file("ash.svg")
         icon.show()
-        box.pack_start(icon)
+        box.pack_start(icon,False,False,10)
         
         label = gtk.Label("Allow Sharing")
         label.show()
         box.pack_start(label)
-        
+        self.ash_box = box
         self.pack_start(box)
         
         # Allow Remixing
@@ -49,17 +50,17 @@ class LicenseWidget(gtk.VBox):
         
         self.ar = gtk.CheckButton()
         self.ar.show()
-        box.pack_start(self.ar)
+        box.pack_start(self.ar,False,False,10)
         
         icon = gtk.Image()
-        icon.set_from_file("/home/scott/.nautilus/python-extensions/ar.svg")
+        icon.set_from_file("ar.svg")
         icon.show()
-        box.pack_start(icon)
+        box.pack_start(icon,False,False,10)
         
         label = gtk.Label("Allow Remixing")
         label.show()
         box.pack_start(label)
-        
+        self.ar_box = box
         self.pack_start(box)
         
         # Prohibit Commercial Works
@@ -68,17 +69,17 @@ class LicenseWidget(gtk.VBox):
         
         self.pcw = gtk.CheckButton()
         self.pcw.show()
-        box.pack_start(self.pcw)
+        box.pack_start(self.pcw,False,False,10)
         
         icon = gtk.Image()
-        icon.set_from_file("/home/scott/.nautilus/python-extensions/pcw.svg")
+        icon.set_from_file("pcw.svg")
         icon.show()
-        box.pack_start(icon)
+        box.pack_start(icon,False,False,10)
         
         label = gtk.Label("Prohibit Commercial Works")
         label.show()
         box.pack_start(label)
-        
+        self.pcw_box = box
         self.pack_start(box)
         
         # Require Others to Share-Alike
@@ -87,16 +88,17 @@ class LicenseWidget(gtk.VBox):
         
         self.sa = gtk.CheckButton()
         self.sa.show()
-        box.pack_start(self.sa)
+        box.pack_start(self.sa,False,False,10)
         
         icon = gtk.Image()
-        icon.set_from_file("/home/scott/.nautilus/python-extensions/sa.svg")
+        icon.set_from_file("sa.svg")
         icon.show()
-        box.pack_start(icon)
+        box.pack_start(icon,False,False,10)
         
         label = gtk.Label("Require Others to Share-Alike")
         label.show()
         box.pack_start(label)
+        self.sa_box = box
         
         self.pack_start(box)
         
@@ -110,7 +112,8 @@ class LicenseWidget(gtk.VBox):
         
         current_license = liblicense.read(filename)
         entry = gtk.Entry()
-        entry.set_text(liblicense.get_name(current_license))
+        version = liblicense.get_version(current_license)
+        entry.set_text(liblicense.get_name(current_license) + " " + str(version[0]) + "." + str(version[1]) + "." + str(version[2]))
         entry.set_editable(False)
         entry.set_has_frame(False)
         entry.show()
@@ -127,10 +130,44 @@ class LicenseWidget(gtk.VBox):
         box.pack_start(label)
         
         current_license = liblicense.read(filename)
+        self.update_checkboxes(current_license)
         entry = gtk.Entry()
         entry.set_text(current_license)
         entry.set_editable(True)
         entry.show()
         box.pack_start(entry)
-        
+        self.label = label
+        self.icon = icon
         self.pack_start(box)
+    
+    def update_checkboxes(self,license):
+        if license=="http://creativecommons.org/licenses/publicdomain/":
+            self.pd.set_active(True)
+        else:
+            permits = liblicense.get_permits(license)
+            self.ash.set_active("http://creativecommons.org/ns#Distribution" in permits)
+            self.pcw.set_active("http://creativecommons.org/ns#CommercialUse" in liblicense.get_prohibits(license))
+            self.ar.set_active("http://creativecommons.org/ns#DerivativeWorks" in permits)
+            self.sa.set_active("http://creativecommons.org/ns#ShareAlike" in liblicense.get_requires(license))
+    
+    def pd_toggled(self,button):
+        self.ash_box.set_sensitive(not button.get_active())
+        self.ar_box.set_sensitive(not button.get_active())
+        self.pcw_box.set_sensitive(not button.get_active())
+        self.sa_box.set_sensitive(not button.get_active())
+
+if __name__=="__main__":
+    import sys
+    if len(sys.argv)<2:
+        print "Please pass a test filename in."
+        sys.exit(1)
+    def close(window):
+        sys.exit(0)
+    window = gtk.Window()
+    window.connect("destroy",close)
+    window.set_title("liblicense widget test")
+    widget = LicenseWidget(sys.argv[1])
+    widget.show()
+    window.add(widget)
+    window.show()
+    gtk.main()
