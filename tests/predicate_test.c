125955cfb0f0455cf0bf3dd193530399704aacc0
paulproteus <paulproteus@aec4f576-4910-0410-beeb-ecb0427469e7>
2008-03-05 20:22:42 +0000

    skeleton tests for predicates
    git-svn-id: https://cctools.svn.sourceforge.net/svnroot/cctools/liblicense/trunk@9284 aec4f576-4910-0410-beeb-ecb0427469e7

---
+++ tests/predicate_test.c
@@ -0,0 +1,42 @@
+/*
+ * Creative Commons has made the contents of this file
+ * available under a CC-GNU-LGPL license:
+ *
+ * http://creativecommons.org/licenses/LGPL/2.1/
+ *
+ * A copy of the full license can be found as part of this
+ * distribution in the file COPYING.
+ *
+ * You may use the liblicense software in accordance with the
+ * terms of that license. You agree that you are solely
+ * responsible for your use of the liblicense software and you
+ * represent and warrant to Creative Commons that your use
+ * of the liblicense software will comply with the CC-GNU-LGPL.
+ *
+ * Copyright 2007-2008, Creative Commons, www.creativecommons.org.
+ * Copyright 2007, Scott Shawcroft.
+ * Copyright (C) 2007 Peter Miller
+ */
+
+#include "liblicense.h"
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <assert.h>
+
+int main(int argc, char** argv) {
+
+	/* First, try to store a license URI in
+	   an MP3 file, which should work fine. */
+
+	/* Then assert that a web statement can
+	   also go in an MP3 file. */
+
+	/* Then assert that a cc:morePermissions can't 
+	   go in an MP3 file */
+
+	/* Then assert that cc:morePermissions can fit in a PDF
+	   with exempi. */
+
+	return 0;
+}
