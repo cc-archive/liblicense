2f14055423b8a1ec3051bc351ef63cad9f942053
paulproteus <paulproteus@aec4f576-4910-0410-beeb-ecb0427469e7>
2008-03-08 01:26:51 +0000

    Does not compile; I wonder why/
    git-svn-id: https://cctools.svn.sourceforge.net/svnroot/cctools/liblicense/trunk@9303 aec4f576-4910-0410-beeb-ecb0427469e7

---
+++ tests/module_selection_test.c
@@ -0,0 +1,45 @@
+/*
+ * Creative Commons has made the contents of this file
+ * available under a CC-GNU-LGPL license:
+ *
+ * http://creativecommons.org/licenses/LGPL/2.1/
+ *
+ * A copy of the full license can be found as part of this
+ * distribution in the file COPYING.
+ *
+ * You may use the liblicense software in accordance with the
+ * terms of that license. You agree that you are solely
+ * responsible for your use of the liblicense software and you
+ * represent and warrant to Creative Commons that your use
+ * of the liblicense software will comply with the CC-GNU-LGPL.
+ *
+ * Copyright 2007, Creative Commons, www.creativecommons.org.
+ * Copyright 2007, Scott Shawcroft.
+ * Copyright (C) 2007 Peter Miller
+ */
+
+#include "liblicense.h"
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <assert.h>
+
+int main() {
+	 /* Assert that an MP3 file uses the MP3 module */
+	 
+	 char* file;
+	 LLModuleDesc * module;
+	 
+	 ll_init();
+	
+	 file = "data/empty.flac";
+	 LLModuleSearchState state = {0};
+	 while(module) {
+		 ll_module_search(file, &state);
+		 printf("%s\n", module->name);
+	 }
+	 
+	 ll_stop();
+	 
+	 return 0;
+}
