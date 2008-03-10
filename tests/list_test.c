fc178e9dfb6e98e9212a0c3e03ed1674443f1631
paulproteus <paulproteus@aec4f576-4910-0410-beeb-ecb0427469e7>
2008-03-10 21:46:08 +0000

    Fixed out-of-bounds access as well as bad testing of mode
    git-svn-id: https://cctools.svn.sourceforge.net/svnroot/cctools/liblicense/trunk@9323 aec4f576-4910-0410-beeb-ecb0427469e7

--- tests/list_test.c
+++ tests/list_test.c
@@ -22,28 +22,41 @@
 
 #include <string.h>
 #include <stdio.h>
+#include <assert.h>
 
 int main(int argc, char** argv) {
+	char * known_good_final_list[] = {"one", "two", "three", "four",
+					  "four", "four", "one", NULL};
 	char** list;
 
         (void)argc;
         (void)argv;
-	list = ll_new_list(5);
+	list = ll_new_list(8);
 	list[0] = strdup("one");
 	list[1] = strdup("two");
 	printf("Current list: ");
 	ll_list_print(list);
 
 	printf("Contains 'one': %d\n", ll_list_contains(list,"one"));
+	assert (ll_list_contains(list,"one"));
 	printf("Contains 'three': %d\n", ll_list_contains(list,"three"));
+	assert (! ll_list_contains(list,"three"));
+
 	list[2] = strdup("three");
 	list[3] = strdup("four");
 	list[4] = strdup("four");
+	list[5] = strdup("four");
+	list[6] = strdup("one");
 	printf("Current list: ");
 	ll_list_print(list);
+	assert (ll_lists_equal(list, known_good_final_list));
 	printf("Index of 'three': %d\n",ll_list_index(list,"three"));
+	assert(ll_list_index(list,"three") == 2);
 	printf("List length: %d\n", ll_list_length(list));
+	assert(ll_list_length(list) == 7);
+	
 	printf("List mode: '%s'\n", ll_list_mode(list,"four"));
+	assert(strcmp(ll_list_mode(list,"four"), "one") == 0);
 
 	printf("List free: \n");
 	ll_free_list(list);
