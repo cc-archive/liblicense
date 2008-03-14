a4b21913a32eda2c168eb0726a9c8667d77cb042
paulproteus <paulproteus@aec4f576-4910-0410-beeb-ecb0427469e7>
2008-03-14 02:26:35 +0000

    Cleaned up, added comments, but no new features, to license.c
    git-svn-id: https://cctools.svn.sourceforge.net/svnroot/cctools/liblicense/trunk@9382 aec4f576-4910-0410-beeb-ecb0427469e7

--- utils/license.c
+++ utils/license.c
@@ -33,7 +33,16 @@ static int remove_webstatement_flag = 0;
 static const int E_NO_DEFAULT_LICENSE=1;
 static const int E_LICENSE_DOES_NOT_VERIFY=2;
 static const int E_COULD_NOT_WRITE=3;
+static const int E_NO_WEB_STATEMENT_SPECIFIED=4;
 
+static int die(const char * error_message_fmt_string,
+	       const char * fmt_string_arg,
+	       int exit_code) {
+  ll_stop();
+  fprintf (stderr, error_message_fmt_string,
+	   fmt_string_arg);
+  return exit_code;
+}
 
 void
 help ()
@@ -43,6 +52,7 @@ help ()
     ("Reads and writes license information (and other related metadata) for a given file or the system default.\n");
   printf ("If options are omitted, uses the default license.\n");
   printf ("If file is omitted, reads and writes the system default license.\n");
+  printf ("NOTE that this tool will only read and write one piece of metadata each time it is run; call it twice if you want to set the license and the web statement URI.\n");
   printf ("\n");
   printf
     ("   -v, --verbose               Outputs more license information.\n");
@@ -75,7 +85,7 @@ help ()
   printf ("\n");
   printf
     ("Exit status is 0 if OK, 1 if no default license is set but the default was");
-  printf ("necessary to proceed, 2 if the given license does not exist, and 3 if there was an error writing a license to a file.\n");
+  printf ("necessary to proceed, 2 if the given license does not exist, and 3 if there was an error writing the metadata to the file, and 4 if you neglected to provide the web statement but asked to set it.\n");
 }
 
 int
@@ -114,159 +124,171 @@ main (int argc, char **argv)
     {"list-all", optional_argument, 0, 'a'},
     {0, 0, 0, 0}
   };
-  int c;
-  int option_index;
+  int write_status = 0;
+  int c = 0;
+  int option_index = 0;
   ll_uri_t license = NULL;
+  ll_uri_t license_read_back = NULL;
   ll_module_t module = NULL;
   char * webstatement = NULL;
+  char * filename = NULL;
+
   ll_init ();
   while (
          (c =
           getopt_long (argc, argv, "vqhmu:l:w:a::", long_options,
-                       &option_index)) != -1)
-    {
-      switch (c)
-        {
-        case 0:
-          continue;
-        case 'h':
-          help ();
-          return 0;
-        case 'm':
-          ll_print_module_info ();
-          return 0;
-        case 'u':
-          if (optarg != NULL)
-            {
-              module = optarg;
-            }
-          break;
-	case 'w':
-	  if (optarg != NULL) 
-	    {
-	      webstatement = optarg;
-	    }
-	  continue;
-        case 'l':
-          if (optarg != NULL)
-            {
-              if (ll_verify_uri (optarg))
-                license = optarg;
-              else
-                {
-                  fprintf (stderr, "Error: License '%s' does not exist.\n",
-                           optarg);
-                  return E_LICENSE_DOES_NOT_VERIFY;
-                }
-            }
-          break;
-        case 'a':
-          if (optarg != NULL)
-            list_juris (optarg);
-          else
-            list_juris (NULL);
-          ll_stop ();
-          return 0;
-        case '?':
-        case 'v':
-          verbose_flag = 1;
-          break;
-        case 'q':
-          verbose_flag = 0;
-          break;
-        default:
-          abort ();
-        }
+                       &option_index)) != -1) {
+    switch (c) {
+    case 0:
+      continue;
+    case 'h':
+      help ();
+      return 0;
+    case 'm':
+      ll_print_module_info ();
+      return 0;
+    case 'u':
+      if (optarg != NULL) {
+	module = optarg;
+      }
+      break;
+    case 'w':
+      if (optarg != NULL) {
+	webstatement = optarg;
+      }
+      continue;
+    case 'l':
+      if (optarg != NULL) {
+	if (ll_verify_uri (optarg)) {
+	  license = optarg;
+	} else {
+	  return die("Error: License '%s' does not exist.\n",
+		     optarg, E_LICENSE_DOES_NOT_VERIFY);
+	}
+      }
+      break;
+    case 'a':
+      if (optarg != NULL) {
+	list_juris (optarg);
+      } else {
+	list_juris (NULL);
+      }
+      ll_stop ();
+      return 0;
+    case '?':
+    case 'v':
+      verbose_flag = 1;
+      break;
+    case 'q':
+      verbose_flag = 0;
+      break;
+    default:
+      abort ();
     }
-  if (set_license_flag)
-    {
-      if (license == NULL)
-        {
-          license = ll_license_default_get ();
-          if (license == NULL)
-            {
-              fprintf (stderr, "Error: No default license set.\n");
-              ll_stop ();
-              return E_NO_DEFAULT_LICENSE;
-            }
-        }
-    }
-  if (argc == optind)
-    {
-      /* No more arguments, assume default. */
-      if (set_flag)
-        {
-          ll_license_default_set (license);
-        }
-      else if (license)
-        {
-          ll_license_print_info (license);
-          return 0;
-        }
-      else
-        {
-          license = ll_license_default_get ();
-        }
-      printf ("The system default license is %s.\n", license);
-      if (verbose_flag)
-        ll_license_print_info (license);
-    }
-  else
-    {
-      /* Next argument is file, use it. */
-      if (remove_license_flag)
-        {
-          license = NULL;
-        }
-      if (set_flag || remove_flag)
-        {
-          int ret;
-          printf ("uri: %s\n", license);
-          if (module)
-            {
-              ret = ll_module_write (argv[optind], LL_LICENSE, license, module);
-            }
-          else
-            {
-              ret = ll_write (argv[optind], LL_LICENSE, license);
-            }
-          if (!ret)
-            {
-              printf ("Unable to write license to file\n");
-	      return E_COULD_NOT_WRITE;
-            }
-        }
-      /* Even if we wrote a license, read it to make sure it worked */
-      if (module)
-        {
-          license = ll_module_read (argv[optind], LL_LICENSE, module);
-        }
-      else
-        {
-          license = ll_read (argv[optind], LL_LICENSE);
-        }
+  }
 
-      if (license)
-        {
-          printf ("%s is licensed under %s\n", argv[optind], license);
-          if (verbose_flag)
-            {
-              ll_license_print_info (license);
-            }
-        }
-      else
-        {
-          if (set_flag)
-            {
-              printf ("Unable to write license to file\n");
-              return E_COULD_NOT_WRITE;
-            }
-          else
-            {
-              printf ("No license found for %s\n", argv[optind]);
-            }
-        }
+  /* Before doing any real processing, do some sanity checks. */
+
+  /* If we were asked to set a license,
+     ensure one was specified, or there is a default. */
+  if (set_license_flag && 
+      license == NULL) {
+    license = ll_license_default_get(); /* We're going to need this later. */
+    if (license /* still */ == NULL) {
+      return die("Error: No default license set.\n", NULL, E_NO_DEFAULT_LICENSE);
     }
+  }
+
+  /* Can't set a web statement if you don't tell me which one. */
+  if (set_webstatement_flag &&
+      webstatement == NULL) {
+    return die("Error: You asked to set the web statement but did not specify the URI to the web statement value you wanted to set.", NULL, E_NO_WEB_STATEMENT_SPECIFIED);
+  }
+  
+  if (argc != optind) {
+    /* There is an extra argument beyond the options,
+       which means a filename was specified. */
+    filename = argv[optind];
+  }
+
+  if (filename == NULL) {
+    /* If you set the web statement operations,
+       you are clearly clueless. */
+    if (set_webstatement_flag || remove_webstatement_flag) {
+      return die("Error: You asked to set or remove the web statement, but you did not specify the filename to operate on.", NULL, E_COULD_NOT_WRITE);
+    }
+    
+    /* If our job was to print information about a license,
+       get that out of the way. */
+    if (!set_license_flag &&
+	license) {
+      ll_license_print_info (license);
+      return 0;
+    }
+
+    /* Otherwise we are operating on the system default license,
+       rather than manipulating a file. */
+    if (set_license_flag) { /* If we were to set it, set it. */
+      ll_license_default_set (license);
+    }
+   
+    /* Always read it and print it. */
+    license = ll_license_default_get ();
+    printf ("The system default license is %s.\n", license);
+
+    if (verbose_flag) { /* Sometimes verbosely. */
+      ll_license_print_info (license);
+    }
+  } else {
+    /* We are supposed to operate on a file. */
+
+    /* First, handle license metadata. */
+    if (remove_license_flag) {
+      license = NULL;
+      set_license_flag = 1; /* A removal is a set to NULL */
+    }
+    if (set_license_flag) {
+      printf ("Setting license to uri: %s\n", license);
+      if (module) {
+	write_status = ll_module_write (filename, LL_LICENSE, license, module);
+      } else {
+	write_status = ll_write (filename, LL_LICENSE, license);
+      }
+      if (write_status < 0) {
+	  printf ("Unable to write license to file\n");
+	  return E_COULD_NOT_WRITE;
+      }
+
+      /* Even if we wrote a license, read it to make sure it worked */
+      if (module) {
+	license_read_back = ll_module_read (argv[optind], LL_LICENSE, module);
+      }
+      else {
+	license_read_back = ll_read (argv[optind], LL_LICENSE);
+      }
+      
+      /* Print the license.  Note that printf will print NULL as
+	 (null), which is great if the user asked to remove a
+	 license. */
+      if (license_read_back == NULL) {
+	printf ("No license found for %s\n", filename);
+
+	/* However, if the license is NULL but we tried
+	   to write a non-NULL license, then we're screwed. */
+	if ( (license != NULL) &&
+	     (license_read_back == NULL) ) {
+	  return die("Unable to write license to file\n", NULL, E_COULD_NOT_WRITE);
+	}
+	
+      } else { 
+	printf ("%s is licensed under %s\n", argv[optind], license);
+	if (verbose_flag) {
+	  ll_license_print_info (license);
+	}
+      }
+    }
+  }
+
   ll_stop ();
   return 0;
 }
