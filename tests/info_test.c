8a13316886e1f494df3f50b6e9bcd658b8caa6a0
Asheesh Laroia <asheesh@asheesh.org>
2008-07-23 21:05:28 -0700

    Split up test_info into more functions

--- tests/info_test.c
+++ tests/info_test.c
@@ -25,32 +25,33 @@
 #include <stdio.h>
 #include <assert.h>
 
-int main(int argc,char** argv) {
-	int known_good_version[] = {2,2,0};
-	char * known_good_prohibits[] = {NULL};
-	char * known_good_permits[] = {LL_DISTRIBUTION, LL_REPRODUCTION, NULL};
-	char * known_good_requires[] = {LL_NOTICE, LL_ATTRIBUTION, NULL};
-	
-	ll_uri_t license;
-	ll_juris_t j;
-	char* name;
-	int* version;
-	ll_uri_t* p;
-	int b;
-
-        (void)argc;
-        (void)argv;
-	ll_init();
+void test_get_attribute_jurisdiction() {
+	char * license;
+	char * j;
 	license = "http://creativecommons.org/licenses/by-nd/2.0/de/";
 	j = ll_get_first(ll_get_attribute(license, LL_JURISDICTION, false));
 	printf("get_jurisdiction: '%s'\n",j);
 	assert (strcmp(j, "http://creativecommons.org/international/de/") == 0);
 	free(j);
+}
+
+void test_get_attribute_name_default_lang() {
+	const char * license;
+	char * name;
+	license = "http://creativecommons.org/licenses/by-nd/2.0/de/";
 	name = ll_get_first(ll_get_attribute(license, LL_NAME, false));
 	printf("get_name: '%s'\n",name);
 	assert (strcmp(name, "Recoñecemento-SenObraDerivada") == 0); /* UTF-8 */
 	free(name);
+}
 
+void test_get_version() {
+	int known_good_version[] = {2,2,0};
+	const char * license;
+	int* version;
+
+	license = "http://creativecommons.org/licenses/by-nd/2.0/de/";
+	
 	version = ll_parse_version(ll_get_first (ll_get_attribute (license, LL_VERSION, false)));
 	printf("get_version: ");
 	assert (ll_int_arrays_equal(version, known_good_version));
@@ -65,6 +66,29 @@ int main(int argc,char** argv) {
 	} else {
 		printf("unversioned\n");
 	}
+}
+
+
+int main(int argc,char** argv) {
+	char * known_good_prohibits[] = {NULL};
+	char * known_good_permits[] = {LL_DISTRIBUTION, LL_REPRODUCTION, NULL};
+	char * known_good_requires[] = {LL_NOTICE, LL_ATTRIBUTION, NULL};
+	
+	ll_uri_t license;
+	ll_juris_t j;
+	char* name;
+	ll_uri_t* p;
+	int b;
+
+        (void)argc;
+        (void)argv;
+	ll_init();
+
+	license = "http://creativecommons.org/licenses/by-nd/2.0/de/";
+
+	test_get_attribute_jurisdiction(license);
+	test_get_attribute_name_default_lang(license);
+	test_get_version();
 
 	p = ll_get_attribute(license, LL_PROHIBITS, false);
 	printf("get_prohibits: ");
