6a4c4b4676e8212e357128cfee2e03c94615e350
Asheesh Laroia <asheesh@asheesh.org>
2009-01-22 16:53:12 -0500

    move more tests in Python to unittest methods

--- bindings/python/tests/test.py
+++ bindings/python/tests/test.py
@@ -17,38 +17,51 @@
 # Copyright 2007, Scott Shawcroft.
 
 import os, sys
-print "Test system_license python bindings:"
+import shutil
+
+print "Test python bindings:"
 
 # Add the build directory to the import path.
 my_path = os.path.abspath(__file__)
 os.stat(my_path) # Assert that we found something.
-sys.path.insert(0, os.path.join(my_path, '..', '.libs'))
+my_dir = os.path.dirname(my_path)
+sys.path.insert(0, os.path.join(my_dir, '.libs'))
 from liblicense import *
 
 import unittest
 import StringIO
 class Tests(unittest.TestCase):
-    def test_juri_none(self):
+    def test_by_nd_data(self):
         license="http://creativecommons.org/licenses/by-nd/2.0/"
         assert get_jurisdiction(license) is None
         assert get_name(license) == 'Attribution-NoDerivs'
+        v = get_version(license)
+        assert v == [2, 0]
+        assert set(get_permits(license)) == set(['http://creativecommons.org/ns#Distribution', 'http://creativecommons.org/ns#Reproduction'])
+        assert set(get_prohibits(license)) == set([])
+        assert set(get_requires(license)) == set(['http://creativecommons.org/ns#Attribution', 'http://creativecommons.org/ns#Notice'])
+        assert verify_uri(license)
+        assert get_attribute(license, "http://purl.org/dc/elements/1.1/title",True) == ['Attribution-NoDerivs']
+        all_licenses = get_licenses()
+        print "\tget_licenses:", all_licenses
+        # FIXME: This fails - why?
+        # assert license in all_licenses
+        # FIXME: Should this really be empty?
+        # print "\tget_licenses(juris):", get_licenses("za")
+
+    def test_write_license_python_bindings(self):
+        # Find us an MP3, first with no module specified
+        shutil.copyfile(os.path.join(my_dir, '../../../tests/write_test.mp3'), 'write_test.mp3')
+        assert (write('write_test.mp3', 'http://creativecommons.org/licenses/by/2.5/au/', None)  == 0)
+        os.unlink('write_test.mp3')
+
+        # Now specify the module
+        shutil.copyfile(os.path.join(my_dir, '../../../tests/write_test.mp3'), 'write_test.mp3')
+        assert (write('write_test.mp3', 'http://creativecommons.org/licenses/by/2.5/au/', 'stub.so') == 0)
+        os.unlink('write_test.mp3')
 
 if __name__ == '__main__':
     unittest.main()
-
-v = get_version(license)
-print "\tget_version: "+".".join(map(str,v))
-print "\tget_permits:", get_permits(license)
-print "\tget_prohibits:", get_prohibits(license)
-print "\tget_requires:", get_requires(license)
-print "\tverify_uri:", verify_uri(license)
-print "\tget_attribute:", get_attribute(license,"http://purl.org/dc/elements/1.1/title",True)
-print "\tget_licenses:", get_licenses()
-print "\tget_licenses(juris):", get_licenses("za")
-
-print "Test write_license python bindings:"
-print "\twrite:",write("license_me.txt","creativecommons.org/licenses/by/2.5/au/")
-print "\twrite(module):",write("license_me.txt","creativecommons.org/licenses/by/2.5/au/","stub.so")
 
 print "Test read_license python bindings:"
 print "\tread:",read("license_me.txt")
